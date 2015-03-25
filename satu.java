c2debc0959c49c28c7f880311af26737053f8b2d
ildaputrinahar <ildaputrinahar@gmail.com>
2015-03-25 19:04:29 +0700

    Pertama
    mengecek Tinggi badan polisi

---
+++ satu.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Ilda Putri on 3/25/2015.
+ */
+public class satu {
+    public static void main(String[] args) {
+
+        float tinggi = 100;
+        if (tinggi < 100) {
+            System.out.println("tidak memenuhi syarat");
+        } else {
+            System.out.println("memenuhi syarat");
+        }
+    }
+}
