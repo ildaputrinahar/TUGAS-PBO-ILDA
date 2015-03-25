ea87a8a354d6e8d0608d90ff633151ef440bc168
ildaputrinahar <ildaputrinahar@gmail.com>
2015-03-25 19:05:29 +0700

    kedua

---
+++ dua.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ilda Putri on 3/25/2015.
+ */
+public class dua {
+    public static void main(String[] args) {
+        float angka=10;
+        if(angka%2==0){
+            System.out.println("genap");
+        }else{
+            System.out.println("ganjil");
+        }
+    }
+}
